d472b75db848b2ae3998d0ff168f7fbba5a4c777
Numan <nomanmani62@gmail.com>
2022-11-30 22:42:11 +0500

    removed links

--- src/components/Links.ts
+++ src/components/Links.ts
@@ -1,22 +1,7 @@
 export default [
   {
     id: 1,
-    name: 'Overview',
-    path: '/',
-  },
-  {
-    id: 2,
-    name: 'Music',
-    path: '/',
-  },
-  {
-    id: 3,
-    name: 'Videos',
-    path: '/',
-  },
-  {
-    id: 4,
-    name: 'Gift Cards',
+    name: 'Live Matches',
     path: '/',
   },
 ];
