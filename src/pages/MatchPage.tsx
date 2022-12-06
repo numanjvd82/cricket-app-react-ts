36f0f2d7c3b665d63e148d72a9fcaec5564ebe65
Numan <nomanmani62@gmail.com>
2022-12-07 00:02:09 +0500

    made a random score number and to update runs function

--- src/pages/MatchPage.tsx
+++ src/pages/MatchPage.tsx
@@ -5,7 +5,55 @@ import { allMatches, Match } from '../constants';
 function MatchPage() {
   const [matches, setMatches] = useState<Match[]>(allMatches);
 
-  console.log(matches);
+  const getRandomNumber = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const updateScore = () => {
+    const newMatches = matches.map((match) => {
+      if (match.status === 'completed') {
+        return match;
+      }
+
+      if (match.team1Overs === 50 && match.team2Overs === 50) {
+        return match;
+      }
+
+      if (match.team1Overs !== 50) {
+        match.team1Runs += getRandomNumber(0, 6);
+      }
+
+      if (match.team2Overs !== 50) {
+        match.team2Runs += getRandomNumber(0, 6);
+      }
+
+      if (match.team2Runs > match.team1Runs) {
+        match.team2Overs = 50;
+        match.status = 'completed';
+      }
+
+      return match;
+    });
+
+    const timer = setTimeout(() => {
+      setMatches(newMatches);
+      console.log('updated');
+    }, 3000);
+
+    if (newMatches.every((match) => match.status === 'completed')) {
+      clearTimeout(timer);
+    }
+
+    return timer;
+  };
+
+  useEffect(() => {
+    updateScore();
+
+    return () => {
+      clearTimeout(updateScore());
+    };
+  }, [matches]);
 
   return (
     <section className="flex flex-col items-center justify-center my-8">
